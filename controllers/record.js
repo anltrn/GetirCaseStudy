8b8d802aeaf20539825c19d1bbee3456eafdf394
Anil Turan <anil.trn45@gmail.com>
2019-11-29 00:50:46 +0300

    getRecords function

---
+++ controllers/record.js
@@ -0,0 +1,43 @@
+const moment = require('moment');
+const Record = require('../models/record')
+
+class CustomError extends Error {  
+  constructor (code, message) {
+    super(message)
+    this.name = this.constructor.name
+    this.code = this.constructor.code
+  }
+}
+
+const getRecords = async (params) => {
+    const startDate = moment(params.startDate, 'YYYY-MM-DD', true)
+    const endDate = moment(params.endDate, 'YYYY-MM-DD', true)
+    const minCount = params.minCount;
+    const maxCount = params.maxCount;
+     if (!startDate.isValid()) {
+            throw new CustomError(1, 'Start Date input is invalid')
+       } else if (!endDate.isValid()) {
+            throw new CustomError(2, 'End Date input is invalid')
+       } else if (typeof minCount != "number" || minCount < 0) {
+            throw new CustomError(3, 'Mincount input is invalid')
+       } else if (typeof maxCount != "number" || maxCount < 0) {
+             throw new CustomError(4, 'Maxcount input is invalid')
+       } else {
+              return Record.aggregate([
+                { $unwind: "$counts" },
+                {
+                    $group: {
+                        _id: "$_id",
+                        key: { $first: "$key" },
+                        createdAt: { $first: "$createdAt" },
+                        totalCount: { $sum: "$counts" }
+                    }
+                },
+                { $project: { "_id": 0 } },
+                {
+                    $match: { "totalCount": { $gte: minCount, $lte: maxCount }, "createdAt": { $gte: startDate.toDate(), $lte: endDate.toDate() } }
+                },
+            ]).exec()
+       }
+}
+module.exports =  {getRecords}
--- routes/records.js
+++ routes/records.js
@@ -1,44 +1,15 @@
 const express = require('express')
 const moment = require('moment')
 const router = express.Router()
-const Record = require('../models/record')
+const RecordController = require('../controllers/record')
+
 
 router.post('/', async (req, res) => {
-    const startDate = moment(req.body.startDate, 'YYYY-MM-DD', true)
-    const endDate = moment(req.body.endDate, 'YYYY-MM-DD', true)
-    const minCount = req.body.minCount;
-    const maxCount = req.body.maxCount;
-    console.log(minCount)
     try {
-        if (!startDate.isValid())
-            res.json({ code: 1, msg: 'Start Date input is invalid' })
-        else if (!endDate.isValid())
-            res.json({ code: 2, msg: "End Date input is invalid" })
-        else if (typeof minCount != "number" || minCount < 0)
-            res.json({ code: 3, msg: "Mincount input is invalid" })
-        else if (typeof maxCount != "number" || maxCount < 0)
-            res.json({ code: 4, msg: "Maxcount input is invalid" })
-        else
-            Record.aggregate([
-                { $unwind: "$counts" },
-                {
-                    $group: {
-                        _id: "$_id",
-                        key: { $first: "$key" },
-                        createdAt: { $first: "$createdAt" },
-                        totalCount: { $sum: "$counts" }
-                    }
-                },
-                { $project: { "_id": 0 } },
-                {
-                    $match: { "totalCount": { $gte: minCount, $lte: maxCount }, "createdAt": { $gte: startDate.toDate(), $lte: endDate.toDate() } }
-                },
-            ]).exec((err, records) => {
-                if (err) throw err;
-                res.json({ code: 0, msg: 'Success', records })
-            })
+        const records = await RecordController.getRecords(req.body)
+        res.json({ code: 0, msg: 'Success', records })
     } catch (err) {
-        res.status(500).json({ message: err.message })
+        res.status(500).json({ code: err.code, msg: err.message})
     }
 })
 
